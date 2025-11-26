75ef485f221167d48e83c7a5b67d0f3b37f2045b
Jakob Wögerbauer <jaki.woe@gmail.com>
2025-11-26 10:24:12 +0100

    Added new Entries

---
+++ AspDotNetTutorial/CinemaApp.Domain/Showtime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CinemaApp.Domain
+{
+    public class Showtime
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public decimal TicketPrice { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+        // Foreign Key: Links this showtime to a specific Movie ID
+        public int MovieId { get; set; }
+
+        // Navigation Property: Allows us to access the full Movie object if needed
+        public Movie? Movie { get; set; }
+    }
+}
