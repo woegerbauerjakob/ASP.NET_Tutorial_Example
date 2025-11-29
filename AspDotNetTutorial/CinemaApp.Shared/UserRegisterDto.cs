67077525f5d6ab90ffb14685cb2483593201a509
Jakob Wögerbauer <jaki.woe@gmail.com>
2025-11-29 11:14:23 +0100

    add DTOs and Controller

---
+++ AspDotNetTutorial/CinemaApp.Shared/UserRegisterDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CinemaApp.Shared
+{
+    public class UserRegisterDto
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
